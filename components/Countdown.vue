96ebe9c90934da217558c24f0a797298de7cb28c
Thomas <thomas.moellers@rwth-aachen.de>
2021-08-27 23:33:34 +0800

    fix: Adjust timer to arrival time

--- components/Countdown.vue
+++ components/Countdown.vue
@@ -51,7 +51,7 @@
         methods: {
             showRemaining() {
                 const timer = setInterval(() => {
-                    const eta = new Date(2021, 12, 3, 20, 30, 00, 00).getTime();
+                    const eta = new Date(2021, 12, 3, 20, 30, 0, 0).getTime();
 
                     const now = new Date().getTime();
 
