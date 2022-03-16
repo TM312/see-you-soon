595c77fac9c7b38fed8893b615cb87c5681b4aba
tm312 <tmoellers@protonmail.ch>
2022-03-16 12:55:59 +0800

    fix: Update itinerary

--- components/Itinerary.vue
+++ components/Itinerary.vue
@@ -79,6 +79,7 @@
                 </dd>
             </div>
 
+            <!-- A & T in DE -->
             <div
                 class="
                     px-4
@@ -116,7 +117,8 @@
                     &#128748; FRA
                 </dd>
             </div>
-
+            
+            
             <div
                 class="
                     px-4
@@ -154,6 +156,86 @@
                     &#128748; SIN
                 </dd>
             </div>
+
+            <!-- M in SG -->
+            <div
+                class="
+                    px-4
+                    py-5
+                    bg-gray-50
+                    shadow
+                    rounded-lg
+                    overflow-hidden
+                    sm:p-6
+                "
+            >
+                <dt class="text-sm font-medium text-sky-500 truncate">
+                    Feb 08, 2022 21:55:00 GMT +02:00
+                </dt>
+                <dd class="mt-1 text-3xl font-semibold text-gray-900">
+                    &#128747; FRA
+                </dd>
+            </div>
+
+            <div
+                class="
+                    px-4
+                    py-5
+                    bg-gray-50
+                    shadow
+                    rounded-lg
+                    overflow-hidden
+                    sm:p-6
+                "
+            >
+                <dt class="text-sm font-medium text-sky-500 truncate">
+                    Feb 09, 2022 17:15:00 GMT +08:00
+                </dt>
+                <dd class="mt-1 text-3xl font-semibold text-gray-900">
+                    &#128748; SIN
+                </dd>
+            </div>
+            
+            
+            <div
+                class="
+                    px-4
+                    py-5
+                    bg-indigo-50
+                    shadow
+                    rounded-lg
+                    overflow-hidden
+                    sm:p-6
+                "
+            >
+                <dt class="text-sm font-medium text-sky-500 truncate">
+                    Mar 06, 2022 23:55:00 GMT +08:00
+                </dt>
+                <dd class="mt-1 text-3xl font-semibold text-indigo-900">
+                    &#128747; KUL
+                </dd>
+            </div>
+
+            <div
+                class="
+                    px-4
+                    py-5
+                    bg-indigo-50
+                    shadow
+                    rounded-lg
+                    overflow-hidden
+                    sm:p-6
+                "
+            >
+                <dt class="text-sm font-medium text-sky-500 truncate">
+                    Mar 07, 2022 06:15:00 GMT +02:00
+                </dt>
+                <dd class="mt-1 text-3xl font-semibold text-indigo-900">
+                    &#128748; FRA
+                </dd>
+            </div>
+
+
         </dl>
     </div>
 </template>
